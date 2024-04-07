3c6f040bfea992a342779f67f8eacee7ff348024
Dilisha727 <dilishapriyashan076@gmail.com>
2024-04-07 14:49:23 +0530

    city ordering

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./styles.css"; // Import the CSS file
+
 const WeatherInfo = ({ weatherData }) => {
+  // Sort the weather data array by temperature in descending order
+  const sortedWeatherData = weatherData.sort((a, b) => b.temperature - a.temperature);
+
   return (
     <div className="weather-info-container">
-      {weatherData.map(({ id, city, temperature, humidity, airPressure, description }) => (
+      {sortedWeatherData.map(({ id, city, temperature, humidity, airPressure, description }) => (
         <div key={id} className="weather-info-item">
           <strong>{city}</strong>
           <br />
